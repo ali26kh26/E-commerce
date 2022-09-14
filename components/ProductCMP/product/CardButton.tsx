b3c928dea10d964f0366d2d05901bbf5fe29a6f6
amir <amir123fg5@gmail.com>
2022-09-14 18:45:55 +0430

    ADD: add animation for productCMP directory

--- components/ProductCMP/product/CardButton.tsx
+++ components/ProductCMP/product/CardButton.tsx
@@ -2,7 +2,7 @@ import {BasketShopping} from '../../icons'
 
 function CardButton(): JSX.Element {
   return (
-    <div className="bg-white p-2.5 rounded-full">
+    <div className="bg-white p-2.5 rounded-full lg:p-4 lg:bg-transparent lg:text-white">
       <BasketShopping className={"fill-gray-700 hover:fill-amber-500 lg:hidden w-6"} />
       <p className="hidden lg:inline">ADD TO CART</p>
     </div>
