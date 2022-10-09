b658a1ecdb94aecf66b66ae5c11c517b7fecde23
ali26kh26 <fcb.akh.26@gmail.com>
2022-10-09 15:27:53 +0330

    ADD: add links to main navigation

--- components/Layout/navbar/navbar.tsx
+++ components/Layout/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 import classes from "./navbar.module.scss";
 import Logo from "../../icons/Logo";
-// import Links from "./links/links";
+import Links from "./links/links";
 import AboutUs from "./about-us/about-us";
 import Options from "./options/options";
 
@@ -13,7 +13,9 @@ const NavBar = () => {
           <li className={classes.logo}>
             <Logo />
           </li>
-          <li className={classes.links}>{/* <Links /> */}</li>
+          <li className={classes.links}>
+            <Links />
+          </li>
           <li className={classes.options}>
             <Options />
           </li>
