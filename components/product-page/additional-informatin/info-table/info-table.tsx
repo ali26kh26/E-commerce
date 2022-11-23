9ea9790d5708267247a22354839b0f987686bd2a
ali26kh26 <fcb.akh.26@gmail.com>
2022-11-23 16:09:36 +0330

    fix: fix table bug

--- components/product-page/additional-informatin/info-table/info-table.tsx
+++ components/product-page/additional-informatin/info-table/info-table.tsx
@@ -4,30 +4,32 @@ import classes from "./info-table.module.scss";
 const InfoTable = ({ product }: { product: product }) => {
   return (
     <table className={classes.table}>
-      <tr>
-        <th>Vendor:</th>
-        <td>vegist</td>
-      </tr>
-      <tr>
-        <th>Name:</th>
-        <td>{product.name}</td>
-      </tr>
-      <tr>
-        <th>Price:</th>
-        <td>${product.price}</td>
-      </tr>
-      <tr>
-        <th>Size:</th>
-        <td>{product.size.toString()}</td>
-      </tr>
-      <tr>
-        <th>Barcode:</th>
-        <td>123456789</td>
-      </tr>
-      <tr>
-        <th>SKU:</th>
-        <td>147852385-15-8</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>Vendor:</th>
+          <td>vegist</td>
+        </tr>
+        <tr>
+          <th>Name:</th>
+          <td>{product.name}</td>
+        </tr>
+        <tr>
+          <th>Price:</th>
+          <td>${product.price}</td>
+        </tr>
+        <tr>
+          <th>Size:</th>
+          <td>{product.size.toString()}</td>
+        </tr>
+        <tr>
+          <th>Barcode:</th>
+          <td>123456789</td>
+        </tr>
+        <tr>
+          <th>SKU:</th>
+          <td>147852385-15-8</td>
+        </tr>
+      </tbody>
     </table>
   );
 };
