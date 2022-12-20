2c457519c5e4b70f266d3d1e8d6b8b4c6f0499b4
ali26kh26 <fcb.akh.26@gmail.com>
2022-12-20 08:44:08 +0330

    fix: add links to collections page

--- components/Layout/navbar/hide-content/hide-content.tsx
+++ components/Layout/navbar/hide-content/hide-content.tsx
@@ -1,5 +1,6 @@
 import classes from "./hide-content.module.scss";
 import { AiOutlineDown } from "react-icons/ai";
+import Link from "next/link";
 
 interface Props {
   title: string;
@@ -10,10 +11,14 @@ const HideContent = ({ title, children }: Props) => {
   return (
     <div className={classes.container}>
       <h2 className={classes.title}>
-        {title}
-        <span>
-          <AiOutlineDown />{" "}
-        </span>
+        <Link href="/collections">
+          <a>
+            {title}
+            <span>
+              <AiOutlineDown />{" "}
+            </span>
+          </a>
+        </Link>
       </h2>
       <div className={classes.content}>{children}</div>
     </div>
